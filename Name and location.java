3bfc996306f90c89fac822ec5ebd8097b06308a0
kpk1719 <131424072+kpk1719@users.noreply.github.com>
2023-05-09 10:54:49 +0530

    Create Name and location.java

---
+++ Name and location.java
@@ -0,0 +1,12 @@
+package sampleproejct;
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Prapul K";
+        String location = "Vijayawada";
+        System.out.println("My name is " + name + " and I work at " + location);
+	}
+
+}
